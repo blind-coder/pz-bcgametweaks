68a4643e18a924c45a469b4579e8f35a46c403aa
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2015-08-08 13:51:21 +0200

    add ability to combine multiple Drainables into one

--- media/lua/client/gametweaks.lua
+++ media/lua/client/gametweaks.lua
@@ -45,3 +45,44 @@ function ISReadABook.start(self) -- {{{
 	BCGT.ISReadABookStart(self);
 end
 -- }}}
+
+BCGT.CombineItemsDoIt = function(item, player, allItems)--{{{
+	local fullType = item:getFullType();
+	local inv = getSpecificPlayer(player):getInventory();
+	local fillstate = 0;
+	for i=0,allItems:size()-1 do
+		fillstate = fillstate + allItems:get(i):getUsedDelta()*100;
+	end
+	while inv:FindAndReturn(fullType) ~= nil do
+		local it = inv:FindAndReturn(fullType);
+		inv:Remove(it);
+	end
+
+	while fillstate > 0 do
+		local it = inv:AddItem(fullType);
+		if fillstate < 100 then
+			it:setUsedDelta(fillstate / 100);
+			fillstate = 0;
+		else
+			fillstate = fillstate - 100;
+		end
+	end
+end
+--}}}
+BCGT.CombineItems = function(player, context, items)--{{{
+	if #items > 1 then return end; -- we only create an entry for the first object
+
+	item = items[1];
+	if not instanceof(item, "InventoryItem") then
+		item = item.items[1];
+	end
+	if item == nil then return end;
+
+	local allItems = getSpecificPlayer(player):getInventory():FindAndReturn(item:getFullType(), 99999);
+	if allItems:size() <= 1 then return end;
+
+	local subMenu = ISContextMenu:getNew(context);
+	local buildOption = context:addOption("Combine into one", item, BCGT.CombineItemsDoIt, player, allItems);
+end
+--}}}
+Events.OnFillInventoryObjectContextMenu.Add(BCGT.CombineItems);
