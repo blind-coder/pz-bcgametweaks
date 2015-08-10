acdca52f6fe6ec739c5c978d9a205b3357259c58
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2015-08-10 15:14:50 +0200

    remove code that has been moved to its own repository

--- media/lua/client/gametweaks.lua
+++ media/lua/client/gametweaks.lua
@@ -30,41 +30,6 @@ ISInventoryTransferAction.isValid = function(self) -- {{{
 end
 -- }}}
 
-BCGT.ISITAperform = ISInventoryTransferAction.perform;
--- Combine Drainable items on transfer
-ISInventoryTransferAction.perform = function(self)--{{{
-	local retVal = BCGT.ISITAperform(self);
-
-	if self.item == nil then return retVal end;
-	if not (instanceof(self.item, "Drainable") or instanceof(self.item, "DrainableComboItem")) then return retVal end
-	if self.item:getReplaceOnDeplete() ~= nil then return retVal end; -- usually water bottles etc.
-
-	local fullType = self.item:getFullType();
-	local inv = self.destContainer;
-	local fillstate = 0;
-	local allItems = inv:FindAndReturn(fullType, 99999);
-	for i=0,allItems:size()-1 do
-		fillstate = fillstate + allItems:get(i):getUsedDelta()*100;
-	end
-	while inv:FindAndReturn(fullType) ~= nil do
-		local it = inv:FindAndReturn(fullType);
-		inv:Remove(it);
-	end
-
-	while fillstate > 0 do
-		local it = inv:AddItem(fullType);
-		if fillstate < 100 then
-			it:setUsedDelta(fillstate / 100);
-			fillstate = 0;
-		else
-			fillstate = fillstate - 100;
-		end
-	end
-
-	return retVal;
-end
---}}}
-
 BCGT.ISReadABookUpdate = ISReadABook.update;
 BCGT.ISReadABookStart = ISReadABook.start;
 -- freeze boredom and unhappyness while reading a skill book
